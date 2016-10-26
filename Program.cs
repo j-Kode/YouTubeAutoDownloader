55c6ec47e1c49a2e232433369010f4252f25b0b6
jKode <jeff.si@cymax.com>
2016-10-25 17:17:28 -0700

    Intial commit, code has been commented out, also required FFMPEG for conversion from MP4 to MP3

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using Supremes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using YoutubeExtractor;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var youtubeLinks = File.ReadLines("C:\\Users\\jeff.si\\Documents\\music.txt");
+
+            ////string metascore = doc.DocumentNode.SelectNodes("//*[@id=\"pl-load-more-destination\"]/tr[1]/td[4]/a")[0].InnerText;
+            ///*
+            // * Get the available video formats.
+            // * We'll work with them in the video and audio download examples.
+            // */
+
+            //IEnumerable<VideoInfo> videoInfos;
+
+            //foreach (var youtubeLink in youtubeLinks)
+            //{
+            //    try
+            //    {
+            //        videoInfos = DownloadUrlResolver.GetDownloadUrls(youtubeLink);
+            //    }
+            //    catch
+            //    {
+            //        continue;
+            //    }
+            //    /*
+            //     * Select the first .mp4 video with 360p resolution
+            //     */
+            //    VideoInfo video = videoInfos
+            //        .First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360);
+
+            //    /*
+            //     * If the video has a decrypted signature, decipher it
+            //     */
+            //    if (video.RequiresDecryption)
+            //    {
+            //        DownloadUrlResolver.DecryptDownloadUrl(video);
+            //    }
+
+            //    /*
+            //     * Create the video downloader.
+            //     * The first argument is the video to download.
+            //     * The second argument is the path to save the video file.
+            //     */
+            //    string str = video.Title;
+            //    Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            //    str = rgx.Replace(str, "");
+
+            //    var videoDownloader = new VideoDownloader(video, Path.Combine("C:\\test\\stuff\\", str + video.VideoExtension));
+
+
+            //    /*
+            //     * Execute the video downloader.
+            //     * For GUI applications note, that this method runs synchronously.
+            //     */
+            //    videoDownloader.Execute();
+            //}
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = @"C:\ffmpeg\bin\ffmpeg.exe";
+
+            var files = Directory.GetFiles("C:\\test\\stuff\\", "*.mp4");
+
+            foreach (var file in files) {
+                startInfo.Arguments = "-i \"" + file + "\" -vn -ar 44100 -ac 2 -ab 192K -f mp3 \"C:\\test\\stuffmp3\\"+ Path.GetFileNameWithoutExtension(file) + ".mp3\"";
+            startInfo.RedirectStandardOutput = true;
+                //startInfo.RedirectStandardError = true;
+
+                Console.WriteLine(string.Format(
+                    "Executing \"{0}\" with arguments \"{1}\".\r\n",
+                    startInfo.FileName,
+                    startInfo.Arguments));
+
+                try
+                {
+                    using (Process process = Process.Start(startInfo))
+                    {
+                        while (!process.StandardOutput.EndOfStream)
+                        {
+                            string line = process.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+                        }
+
+                        process.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YouTubeServiceClient.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Requests;
+
+namespace ConsoleApplication2
+{
+    public class YouTubeServiceClient
+    {
+        private static YouTubeServiceClient instance;
+        public static YouTubeServiceClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new YouTubeServiceClient();
+                }
+                return instance;
+            }
+        }
+        public async Task<YouTubeService> GetYouTubeService(string userEmail)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("C:\\test\\stuff.json", FileMode.Open, FileAccess.Read))
+            {
+                dsAuthorizationBroker.RedirectUri = "http://localhost:64987/authorize/";
+
+                credential = await dsAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.Load(stream).Secrets,
+                new[]
+                {
+                    YouTubeService.Scope.Youtube,
+                    YouTubeService.Scope.Youtubepartner,
+                    YouTubeService.Scope.YoutubeUpload,
+                    YouTubeService.Scope.YoutubepartnerChannelAudit,
+                    YouTubeService.Scope.YoutubeReadonly
+                },
+                userEmail,
+                CancellationToken.None,
+                new FileDataStore(this.GetType().ToString()));
+            }
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = this.GetType().ToString()
+            });
+            return youtubeService;
+        }
+        public class dsAuthorizationBroker : GoogleWebAuthorizationBroker
+        {
+            public static string RedirectUri;
+
+            public new static async Task<UserCredential> AuthorizeAsync(
+                ClientSecrets clientSecrets,
+                IEnumerable<string> scopes,
+                string user,
+                CancellationToken taskCancellationToken,
+                IDataStore dataStore = null)
+            {
+                var initializer = new GoogleAuthorizationCodeFlow.Initializer
+                {
+                    ClientSecrets = clientSecrets,
+                };
+                return await AuthorizeAsyncCore(initializer, scopes, user,
+                    taskCancellationToken, dataStore).ConfigureAwait(false);
+            }
+
+            private static async Task<UserCredential> AuthorizeAsyncCore(
+                GoogleAuthorizationCodeFlow.Initializer initializer,
+                IEnumerable<string> scopes,
+                string user,
+                CancellationToken taskCancellationToken,
+                IDataStore dataStore)
+            {
+                initializer.Scopes = scopes;
+                initializer.DataStore = dataStore ?? new FileDataStore(Folder);
+                var flow = new dsAuthorizationCodeFlow(initializer);
+                return await new AuthorizationCodeInstalledApp(flow,
+                    new LocalServerCodeReceiver())
+                    .AuthorizeAsync(user, taskCancellationToken).ConfigureAwait(false);
+            }
+        }
+        public class dsAuthorizationCodeFlow : GoogleAuthorizationCodeFlow
+        {
+            public dsAuthorizationCodeFlow(Initializer initializer)
+                : base(initializer) { }
+
+            public override AuthorizationCodeRequestUrl
+                           CreateAuthorizationCodeRequest(string redirectUri)
+            {
+                return base.CreateAuthorizationCodeRequest(dsAuthorizationBroker.RedirectUri);
+            }
+        }
+
+    }
+}
